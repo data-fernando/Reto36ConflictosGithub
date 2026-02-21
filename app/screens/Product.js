3636cacd2044bebc3a806b1d3cd4b77faba7ce01
Fernando G. Ponce <data.fernan@gmail.com>
2026-02-20 21:46:46 -0500

    pull y push estudiante 2 , fondo homescrion y cambio de color botones a rojo

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -6,7 +6,7 @@ export const Product = ({navigation}) =>{
         <Button
             title='HOME'
             onPress={()=>{
-                navigation.navigate('WelcomeNav');
+                navigation.navigate('HomeNav');
             }}
         />
     </View>
