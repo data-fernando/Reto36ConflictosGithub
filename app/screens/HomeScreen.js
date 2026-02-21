5014454eb545cd5f6227e11f824af4d66139c827
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-02-20 22:32:52 -0500

    cambio de etiqueta en contactos

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -11,7 +11,7 @@ export const Home = ({ navigation }) => {
                 <Button
                     title='CONTACTS'
                     onPress={() => {
-                        navigation.navigate('PhoneNumberNav');
+                        navigation.navigate('CallNumber');
                     }}
                 />
             </View>
