21bbe9bfe309879f164e7e31b60a84c405d901de
Fernando G. Ponce <data.fernan@gmail.com>
2026-02-20 22:22:05 -0500

    cambio

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -11,7 +11,7 @@ export const Home = ({ navigation }) => {
                 <Button
                     title='CONTACTS'
                     onPress={() => {
-                        navigation.navigate('PhoneNumberNav');
+                        navigation.navigate('PhoneNumberNav1');
                     }}
                 />
             </View>
