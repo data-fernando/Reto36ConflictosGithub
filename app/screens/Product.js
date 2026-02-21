79e3a0c31d485ef8106d85ad357bce8b01a0819d
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-02-20 21:59:58 -0500

    estudiante 1, cambio de color en prductos y cambio de color en el fondo general

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,7 +2,10 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text>Bienvenido a mi tienda, soy Marcelo Salcedo</Text>
+        <Text style={styles.prod1}>Pantalla</Text>
+        <Text style={styles.prod2}>Teclado</Text>
+        <Text style={styles.prod3}>CPU</Text>
         <Button
             title='HOME'
             onPress={()=>{
@@ -16,8 +19,18 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#f6ff00',
       alignItems: 'center',
       justifyContent: 'center',
     },
+
+    prod1: {
+        color:'#ef1212',
+    },
+    prod2: {
+        color:'#1216ef',
+    },
+    prod3: {
+        color:'#28ef12',
+    }
   });
